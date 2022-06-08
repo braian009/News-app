cf06f0a499aaf6d1ee482033e0125d7fff177b29
braian <braianyanquin@gmail.com>
2022-06-08 15:26:03 -0300

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+# accounts/views.py
+
+from django.shortcuts import render
+from .forms import CustomUserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
